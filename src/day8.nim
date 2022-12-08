f7e3b6878c7194f246ade4f47807217659771e5b
David Miotti <david@station-coding.com>
2022-12-08 13:29:55 +0100

    Day 8: part 2

--- src/day8.nim
+++ src/day8.nim
@@ -1,50 +1,11 @@
 import math, strutils
 
+
 type
-    Tree = int
     Forest = object
-        trees: seq[Tree]
+        trees: seq[int]
         size: int
-    Position = int
     Direction = enum north, south, east, west
-
-
-proc isTreeVisible(forest: Forest, pos: Position): bool =
-    # is on the edge.
-    if pos mod forest.size == 0 or (pos+1) mod forest.size == 0 or 
-        pos < forest.size or pos > forest.size ^ 2 - forest.size:
-            return true
-
-    let
-        tree = forest.trees[pos]
-        stepsDirections = {
-            north: pos div forest.size,
-            south: forest.size - ((pos div forest.size) + 1),
-            east: pos mod forest.size,
-            west: forest.size - (pos mod forest.size) - 1,
-        }
-
-    for (direction, steps) in stepsDirections:
-        var visible = true
-        for i in 1..steps:
-            let cmpPos = case direction:
-                of north: pos - i * forest.size
-                of south: pos + i * forest.size
-                of east: pos - i
-                of west: pos + i
-            if forest.trees[cmpPos] >= tree:
-                visible = false
-                break
-        if visible:
-            return true
-
-
-proc `$`(forest: Forest): string =
-    for i, c in forest.trees:
-        result &= (if isTreeVisible(forest, i): "(+" else: "(-")
-        result &= $c & ")"
-        if (i+1) mod forest.size == 0:
-            result &= "\n"
 
 
 proc newForest(filename: string): Forest =
@@ -55,13 +16,66 @@ proc newForest(filename: string): Forest =
             result.trees.add(parseInt($c))
 
 
-proc countVisibleTrees(forest: Forest): int =
-    for i in 0..forest.trees.len-1:
+func positionMovingInDirection(forest: Forest, pos: int, steps: int, direction: Direction): int =
+    case direction:
+    of north: pos - steps * forest.size
+    of south: pos + steps * forest.size
+    of east: pos - steps
+    of west: pos + steps
+
+
+func maxStepsInDirection(forest: Forest, pos: int, direction: Direction): int =
+    case direction:
+    of north: pos div forest.size
+    of south: forest.size - ((pos div forest.size) + 1)
+    of east: pos mod forest.size
+    of west: forest.size - (pos mod forest.size) - 1
+
+
+func isTreeVisible(forest: Forest, pos: int): bool =
+    # is on the edge.
+    if pos mod forest.size == 0 or (pos+1) mod forest.size == 0 or 
+        pos < forest.size or pos > forest.size ^ 2 - forest.size:
+            return true
+
+    for direction in [north, south, east, west]:
+        var visible = true
+        for step in 1..forest.maxStepsInDirection(pos, direction):
+            let cmpPos = forest.positionMovingInDirection(pos, step, direction)
+            if forest.trees[cmpPos] >= forest.trees[pos]:
+                visible = false
+                break
+        if visible:
+            return true
+
+
+func countVisibleTrees(forest: Forest): int =
+    for i in forest.trees.low..forest.trees.high:
         if isTreeVisible(forest, i):
             result += 1
 
 
+func viewingDistance(forest: Forest, pos: int, direction: Direction): int =
+    let tree = forest.trees[pos]
+
+    for step in 1..forest.maxStepsInDirection(pos, direction):
+        result += 1
+        let cmpPos = forest.positionMovingInDirection(pos, step, direction)
+        if forest.trees[cmpPos] >= tree:
+            break
+
+
+func highestScenicScore(forest: Forest): int =
+    for pos in forest.trees.low..forest.trees.high:
+        let 
+            atNorth = forest.viewingDistance(pos, north)
+            atSouth = forest.viewingDistance(pos, south)
+            atEast = forest.viewingDistance(pos, east)
+            atWest = forest.viewingDistance(pos, west)
+            score = atNorth * atSouth * atEast * atWest
+        if score > result: result = score
+
+
 proc run*(filename: string): (int, int) =
     let forest = newForest(filename)
-    result[0] = countVisibleTrees(forest)
-    echo forest
+    (forest.countVisibleTrees, forest.highestScenicScore)
--- tests/test_day8.nim
+++ tests/test_day8.nim
@@ -2,4 +2,4 @@ import unittest
 import ../src/day8
 
 
-test "run": check run("./tests/inputs/day8.txt") == (21, 0)
+test "run": check run("./tests/inputs/day8.txt") == (21, 8)
