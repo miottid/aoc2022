5eb6ff5b984556486a51e213de680a65a67446af
David Miotti <david@station-coding.com>
2022-12-11 12:24:23 +0100

    Day 2: part 2

--- src/day11.nim
+++ src/day11.nim
@@ -1,77 +1,85 @@
-import algorithm, math, sequtils, strscans, strutils
+import algorithm, sequtils, strscans, strutils
 
 
-type
-    Monkey = ref object
-        items: seq[int]
-        instruction: string
-        divisibleBy, whenTrue, whenFalse: int
+type Monkey = ref object
+    starting: seq[int]
+    operation: (string, string)
+    divisibleBy, whenTrue, whenFalse: int
 
 
-proc worryLevel(instruction: string, item: int): int =
-    let
-        op = if instruction.contains("*"): "*" else: "+"
-        parts = instruction.split(" " & $op & " ")
-        right = try: parseInt(parts[1]) except: item
-        level = if op == "*": item * right else: item + right
-    floor(level / 3).toInt
-
-
-proc processRound(monkeys: seq[Monkey]): seq[int] =
-    for m in monkeys:
-        result.add(m.items.len)
-        for item in m.items:
-            let
-                worryLevel = worryLevel(m.instruction, item)
-                division = worryLevel / m.divisibleBy
-                isDivisible = division - division.toInt.toFloat == 0
-                targetMonkey =
-                    if isDivisible: m.whenTrue
-                    else: m.whenFalse
-
-            monkeys[targetMonkey].items.add(worryLevel)
-
-        m.items = @[]
+const
+    testMatch = "Test: divisible by $i"
+    testTrueMatch = "If true: throw to monkey $i"
+    testFalseMatch = "If false: throw to monkey $i"
 
 
 proc parseMonkeys(filename: string): seq[Monkey] =
-    var lineNumber: int = 0
+    var i: int = 0
     for line in lines(filename):
-        let ln = line.strip()
-        if lineNumber mod 7 == 0:
-            result.add(Monkey())
-        elif lineNumber mod 7 == 1:
+        let
+            ln = line.strip()
+            cursor = i mod 7
+
+        case cursor:
+        of 0: result.add(Monkey())
+        of 1:
+            let parts = ln.split(": ")
+            result[result.high].starting = parts[1]
+                .split(", ").mapIt(it.parseInt)
+        of 2:
             let
-                parts = ln.split(": ")
-                items = parts[1].split(", ").map(parseInt)
-            result[result.high].items = items
-        elif lineNumber mod 7 == 2:
-            let parts = ln.split(" = ")
-            result[result.high].instruction = parts[1]
-        elif lineNumber mod 7 == 3:
+                tmp = ln.split(" = ")
+                op = if tmp[1].contains("*"): "*" else: "+"
+                parts = tmp[1].split($op)
+            result[result.high].operation = (op, parts[1].strip())
+        of 3:
             var divisibleBy: int
-            discard ln.scanf("Test: divisible by $i", divisibleBy)
+            discard ln.scanf(testMatch, divisibleBy)
             result[result.high].divisibleBy = divisibleBy
-        elif lineNumber mod 7 == 4:
+        of 4:
             var whenTrue: int
-            discard ln.scanf("If true: throw to monkey $i", whenTrue)
+            discard ln.scanf(testTrueMatch, whenTrue)
             result[result.high].whenTrue = whenTrue
-        elif lineNumber mod 7 == 5:
+        of 5:
             var whenFalse: int
-            discard ln.scanf("If false: throw to monkey $i", whenFalse)
+            discard ln.scanf(testFalseMatch, whenFalse)
             result[result.high].whenFalse = whenFalse
+        else: discard
 
-        lineNumber += 1
+        i += 1
 
 
-proc run*(filename: string): (int, int) =
-    let monkeys = parseMonkeys(filename)
+func monkeyBusiness(monkeys: seq[Monkey], round: int, divideByThree = true): int =
+    let lcm = monkeys.mapIt(it.divisibleBy).foldl(a*b)
+    var counts = newSeq[int](monkeys.len)
 
-    var monkeysInspections = newSeq[int](monkeys.len)
-    for i in 0..<20:
-        let inspections = processRound(monkeys)
-        for i, nb in inspections:
-            monkeysInspections[i] += nb
+    for _ in 0..<round:
+        for i, m in monkeys:
+            counts[i] += m.starting.len
 
-    let mostActiveMonkeys = monkeysInspections.sorted(SortOrder.Descending)
-    (mostActiveMonkeys[0..1].foldl(a * b), 0)
+            for item in m.starting:
+                let
+                    right =
+                        if m.operation[1] == "old": item
+                        else: parseInt(m.operation[1])
+                    level =
+                        if m.operation[0] == "*": item * right
+                        else: item + right
+                    normLevel =
+                        if divideByThree: level div 3
+                        else: level mod lcm
+                    divisible = normLevel mod m.divisibleBy == 0
+                    target =
+                        if divisible: m.whenTrue
+                        else: m.whenFalse
+
+                monkeys[target].starting.add(normLevel)
+
+            m.starting = @[]
+
+    counts.sorted(Descending)[0..1].foldl(a * b)
+
+
+proc run*(filename: string): (int, int64) =
+    result[0] = monkeyBusiness(parseMonkeys(filename), 20)
+    result[1] = monkeyBusiness(parseMonkeys(filename), 10_000, false)
